a8936201fe600b9bf5b44b4f84c34257c78309d0
Leibnitz Rojas <leiparov@gmail.com>
2021-12-14 00:30:46 -0500

    framework package

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="execution-framework",
+    version="0.0.1",
+    author="Movistar",
+    author_email="leibnitz.rojas@telefonica.com",
+    description="Framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/advanced-analytics-tdp/execution-framework',
+    project_urls={
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.6",
+)
